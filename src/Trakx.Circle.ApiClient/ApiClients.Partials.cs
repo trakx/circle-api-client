f73b3561135ca40e42785f1fa645c376fe7fa0d9
monsieurleberre <monsieur.leberre@gmail.com>
2021-02-17 08:43:26 +0000

    trigger rebuild

--- src/Trakx.Circle.ApiClient/ApiClients.Partials.cs
+++ src/Trakx.Circle.ApiClient/ApiClients.Partials.cs
@@ -1,10 +1,11 @@
 ﻿
-
 namespace Trakx.Circle.ApiClient
 {
     internal partial class AccountsClient
     {
+#pragma warning disable S1172
         partial void PrepareRequest(System.Net.Http.HttpClient client, System.Net.Http.HttpRequestMessage request, string url)
+#pragma warning restore S1172
         {
             CredentialProvider.AddCredentials(request);
         }
